a50c8e1e22e3f3cf7642a91966cadc027b58f517
JScorpio67 <102548646+JScorpio67@users.noreply.github.com>
2024-05-06 18:42:31 +0200

    06052024II
    FireBase, antes de instalar primeng

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,2 +1,9 @@
 export interface User {
+    //INTERFAZ DE FIREBASE
+    uid?: string,
+    email: string,
+    displayName: string,
+    photoURL?: string,
+    //nos permite saber si el email ha sido verificado
+    emailVerified: boolean,    
 }
